bf13762bb9ce3644b4f7d4db0d00f52ca4840bcd
LucasFM4 <90008115+LucasFM4@users.noreply.github.com>
2021-09-21 10:09:13 -0300

    Add files via upload

--- aula 16/aula_16_10.py
+++ aula 16/aula_16_10.py
@@ -1,14 +1,7 @@
-galera = list()
-dado = list()
-totmai = totmen = 0
-for c in range (0, 3):
-	dado.append(str(input('Nome: ')))
-	dado.append(int(input('Idade: ')))
-	galera.append(dado[:])
-	dado.clear()
-
-for p in galera:
-	if p[1] >= 21:
-		print('{p[0]} é maior de Idade.')
-	else:
-		print('{p[0]} é maior de Idade.')
+num = (2, 5, 9, 1)
+# num[2] = 3
+num.append(7)
+num.sort(reverse=true)
+num.insert(2, 2)
+print (num)
+print('essa lista tem {len(num) elementos.}')
