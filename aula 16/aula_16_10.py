71207e210b9c348cc1fdc3858d0a85a198b6e334
LucasFM4 <90008115+LucasFM4@users.noreply.github.com>
2021-09-22 14:34:12 -0300

    laço de repetição for

--- aula 16/aula_16_10.py
+++ aula 16/aula_16_10.py
@@ -1,7 +1,8 @@
-num = (2, 5, 9, 1)
-# num[2] = 3
+num = [2, 5, 9, 1]
+num[2] = 3
 num.append(7)
-num.sort(reverse=true)
+num.sort(reverse=True)
 num.insert(2, 2)
-print (num)
-print('essa lista tem {len(num) elementos.}')
+num.remove(2)
+print(num)
+print('Essa lista tem {len(num)} elementos.')
