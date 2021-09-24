609908c550e103a8d55b3542573cd46d6d7aab34
LucasFM4 <90008115+LucasFM4@users.noreply.github.com>
2021-09-24 11:16:09 -0300

    trabalhando com vetores escolhendo o maior  e o menor numero

---
+++ aula 17/aula_17_2.py
@@ -0,0 +1,16 @@
+listanum = []
+mai = 0
+men = 0
+for c in range(0, 5):
+	listanum.append(int(input(f'Digite um valor para a posição{c}: ')))
+	if c == 0:
+		mai = men = listanum[c]
+	else:
+		if listanum[c] > mai:
+			mai = listanum[c]
+		if listanum[c] < men:
+			men = listanum[c]
+print('(=-)' * 30)
+print(f'Você digitou os valores{listanum}')
+print(f'O maior valor digitado foi {mai}')
+print(f'O menor valor digitado foi {men}')
