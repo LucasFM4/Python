110f1afdb2eecdc3a621d9557eacc894f492d54a
LucasFM4 <90008115+LucasFM4@users.noreply.github.com>
2021-09-29 10:07:44 -0300

    trabalhando com bibliotecas mostrando as idades e nome de pessoas

--- aula 18/aula_18.py
+++ aula 18/aula_18.py
@@ -3,5 +3,8 @@ teste.append('Bom Dia')
 teste.append('Lucas')
 teste.append(25)
 galera = list()
-galera.append(teste)
+galera.append(teste[:])
+teste[0] = 'Davi'
+teste[0] = 23
+galera.append(teste[:])
 print(galera)
