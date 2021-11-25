faca0a512cee2ca7fcd8d43007523d1268005f50
LucasFM4 <90008115+LucasFM4@users.noreply.github.com>
2021-11-24 22:44:19 -0300

    listas

---
+++ aula 19/testeamor.py
@@ -0,0 +1,9 @@
+brasil = list()
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+estado3 = {'uf': 'Santa Catarina', 'sigla': 'SC'}
+brasil.append(estado1)
+brasil.append(estado2)
+brasil.append(estado3)
+
+print(brasil)
