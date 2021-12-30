0cdd607d4b8ecd5fe3fb14b235a32df97ceb7e59
Lucas Munzlinger Ferreira <90008115+LucasFM4@users.noreply.github.com>
2021-12-30 08:28:10 -0300

    programas com soma e título

---
+++ aula 20/aula_20_2.py
@@ -0,0 +1,22 @@
+def soma(a, b):
+	print(f'A = {a} e B = {b}')
+	s = a + b
+	print(f'A soma A + b = {s}')
+
+
+#programa principal
+soma(4, 5)
+soma(8, 9)
+soma(2, 1)
+
+
+def titulo(txt):
+    print('=' *38)
+    print(txt)
+    print('=' *38)
+
+
+# programa Principal
+titulo(' Aprenda Python  ')
+titulo(' Python e Muito Bom  ')
+titulo(' Lucas F.M  ')
