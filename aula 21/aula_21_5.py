ddbe2d1168050092f5522038e8620e0dc61ddb5d
Lucas Munzlinger Ferreira <90008115+LucasFM4@users.noreply.github.com>
2022-01-17 15:41:11 -0300

    trabalhando com somador

---
+++ aula 21/aula_21_5.py
@@ -0,0 +1,13 @@
+def somar(a=0, b=0, c=0):
+	"""
+	-> Faz uma soma de três valores e mostra o resultado na tela
+	:param a: o primeiro valor
+	:param b: o segundo valor
+	:param c:o terceiro valor
+	Função criada por Lucas Ferreira Munzlinger do cana Lucas_H_D.
+	"""
+	s = a + b + c
+	print(f'A soma vale {s}')
+
+
+somar(b=1, c=1)
