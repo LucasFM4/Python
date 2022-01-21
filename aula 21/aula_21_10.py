34053a3794faa077a8235f818b9cad504110f2cd
Lucas Munzlinger Ferreira <90008115+LucasFM4@users.noreply.github.com>
2022-01-21 09:41:56 -0300

    trabalhando com somar

---
+++ aula 21/aula_21_10.py
@@ -0,0 +1,12 @@
+def somar(a=0, b=0, c=0):
+	s = a + b + c
+	return s
+
+
+R1 = somar(9, 7, 3)
+R2 = somar(6, 8, 2)
+R3 = somar(2, 6, 9)
+R4 = somar(2, 4 ,7)
+R5 = somar(4, 7 ,9)
+
+print(f'osresultados foram {R1}, {R2}, {R3}, {R4} e {R5}')
