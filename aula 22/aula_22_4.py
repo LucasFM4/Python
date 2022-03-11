85916b6c570cc32ab6693eca5d7f8879795b8ae6
Lucas Munzlinger Ferreira <90008115+LucasFM4@users.noreply.github.com>
2022-03-11 14:06:45 -0300

    trabalhando com titulo

---
+++ aula 22/aula_22_4.py
@@ -0,0 +1,10 @@
+def titulo(txt):
+	print('=-' * 30)
+	print(txt)
+	print('=-' * 30)
+
+
+#programa principal
+titulo('      Lucas Games  ')
+titulo('      se unscreva  ')
+titulo('      e deixa o like  ')
