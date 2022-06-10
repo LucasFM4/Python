7cbe973bcbfbc8ef44d83df015f3c1aefe1f65d9
Lucas Munzlinger Ferreira <90008115+LucasFM4@users.noreply.github.com>
2022-06-10 14:54:38 -0300

    teste

---
+++ aula_23_2.py
@@ -0,0 +1,11 @@
+def teste():
+	x = 8
+	print (f'Na função teste, n vale {n}')
+	print (f'Na função teste, x vale {x}')
+
+
+# PROGRAMA PRINCIPAL
+n = 2
+print(f'No programa principal, n vale {n}')
+teste()
+print(f'No programa principal, x vale {x}')
